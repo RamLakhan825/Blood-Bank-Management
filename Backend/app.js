3bfbbd06e302b211c8e2d6cd14bd8c67b5b19244
RamLakhan825 <rljangid825@gmail.com>
2025-02-27 22:21:14 +0530

    modified:   Backend/app.js

--- Backend/app.js
+++ Backend/app.js
@@ -11,9 +11,9 @@ app.use(cors());
 
 // JSON
 app.use(express.json());
-app("/",(req,res)=>{
-    res.send("Welcome to the Donor Management System");
-})
+// app("/",(req,res)=>{
+//     res.send("Welcome to the Donor Management System");
+// })
 // ROUTES
 app.use("/api/v1/auth",authRoute);
 app.use("/api/v1/donors", donorRoute);
