c2229aa870d242f3a0f4591a85be8fa1efede6bd
RamLakhan825 <rljangid825@gmail.com>
2025-02-27 22:29:00 +0530

    modified:   Frontend/src/requestMethods.js

--- Frontend/src/requestMethods.js
+++ Frontend/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://blood-bank-project-8lri.onrender.com";
+const BASE_URL = "https://blood-bank-project-8lri.onrender.com/api/v1/";
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
